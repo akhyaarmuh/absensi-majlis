83e5ed040249d29c15b85b3cb943892c2e7d0b15
Akhyar Muhammad <akhyaarmuh@gmail.com>
2023-08-18 10:41:02 +0800

    update database

--- update_v2.js
+++ update_v2.js
@@ -2,11 +2,7 @@ import 'dotenv/config';
 import './config/database.js';
 import Member from './models/Member.js';
 
-// cari semua member
-const members = await Member.find();
-
-for (const member of members) {
-  await Member.findByIdAndUpdate({ _id: member._id }, { absent_dzikiran: [] });
-}
+// update semua member
+await Member.updateMany({ absent_dzikiran: [] });
 
 console.log('Berhasil');
